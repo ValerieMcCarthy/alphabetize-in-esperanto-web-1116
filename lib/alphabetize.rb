2d796e04110cd5e5653665ecc92f149dd03e09c7
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-22 16:51:40 -0500

    Alphabetized.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require "pry"
+
+
 def alphabetize(arr)
-  # code here
+  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = esp_alph.split("").map { |letter| letter }
+
+  arr.sort_by do |word|
+  	word = word.split("")
+  	word.map do |letter|
+  		alphabet.index(letter)
+  	end
+  end
 end
+
